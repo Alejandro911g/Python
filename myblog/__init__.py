ddbbc0ec09673fd35b9f1bc6b93bc5b455720f63
Alejandro911g <dash_verduzco@hotmail.com>
2022-05-19 19:50:23 -0500

    primer commit

---
+++ myblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#Cargar las configuraciones
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
+
+#Importar vistas
+from myblog.views.auth import auth
+app.register_blueprint(auth)
+
+from myblog.views.auth import auth
+app.register_blueprint(auth)
+
+db.create_all()
